43372faec1efd450d76c9350729d33aca5a812a7
fahimshahariar09 <fahimshahariar20002@gmail.com>
2024-08-26 02:39:53 +0600

    https://github.com/fahimshahariar09/classicit_getx_api_design_practice_all.git

---
+++ lib/controller/api_controller/product/product_detalis.dart
@@ -0,0 +1,27 @@
+
+import 'dart:convert';
+import 'dart:developer';
+import 'package:getx_api_design_practice/controller/api_controller/apis.dart';
+import 'package:getx_api_design_practice/model/product_detalis_model.dart';
+import 'package:http/http.dart' as http;
+
+class ProductDetalisService{
+  static Future<ProductDetails?> productDetalisService({required String id})async{
+
+    try{
+
+      Uri url = Uri.parse(Apis.productdetalis);
+
+      var response = await http.get(url);
+      if(response.statusCode==200){
+        ProductDetalisModel model = ProductDetalisModel.fromJson(jsonDecode(response.body));
+        return model.productDetails;
+      }
+
+    }catch (e){
+      log("error :$e");
+    }
+    return null;
+    
+  }
+}
