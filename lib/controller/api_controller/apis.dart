39a3afb9b14a5ea6c2a16ab9c728af6d15addcc6
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-09 20:12:14 -0700

    first commit

--- lib/controller/api_controller/apis.dart
+++ lib/controller/api_controller/apis.dart
@@ -5,12 +5,12 @@ class Apis{
 
   static String baseIp ="";
   static String register ="";
-  static String login ="$baseIp/api/login";
-  static String productshow ="$baseIp/api/app/v1/products";
-  static String productdetalis ="$baseIp/api/app/v1/products/show";
-  static String productshowcart ="$baseIp/api/app/v1/cart/list";
-  static String productaddtocart ="$baseIp/api/app/v1/add-to-cart";
-  static String productdeletecart ="$baseIp/api/app/v1/cart/delete";
+  static String login ="";
+  static String productshow ="";
+  static String productdetalis ="";
+  static String productshowcart ="";
+  static String productaddtocart ="";
+  static String productdeletecart ="";
 
 
 }
